9ba87b106bfafb8ef18f14f3667da41652642149
knupfer <fknupfer@gmail.com>
2024-09-04 12:46:44 +0200

    add incremental builds

--- default.nix
+++ default.nix
@@ -1,9 +1,14 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 
 let
 
   inherit (nixpkgs) pkgs;
-
+  inherit (pkgs.haskell.lib.compose) overrideCabal;
+  
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+  
   f = { mkDerivation, base, bytestring, lib, process, time }:
       mkDerivation {
         pname = "minstatus";
@@ -16,15 +21,16 @@ let
         license = lib.licenses.bsd3;
         mainProgram = "minstatus";
       };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
+ 
+  drv = haskellPackages.callPackage f {};
+  drv-inc-out = overrideCabal (drv: {
+    doInstallIntermediates = true;
+    enableSeparateIntermediatesOutput = true;
+  }) drv;
+  drv-inc = overrideCabal (drv: {
+    previousIntermediates = drv-inc-out.intermediates;
+  }) drv;
+  
 in
 
-  if pkgs.lib.inNixShell then drv.env else drv
+  if pkgs.lib.inNixShell then drv-inc.env else drv-inc
