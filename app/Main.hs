c2abf5fe3b046b432fb763a0193b666e04bd52b6
knupfer <fknupfer@gmail.com>
2024-09-07 15:50:53 +0200

    clean up

--- app/Main.hs
+++ app/Main.hs
@@ -26,9 +26,9 @@ main = do
   zone <- getTimeZone =<< getCurrentTime
   initialState <- State <$> getTime zone <*> getSound <*> getBattery
   updater <- newEmptyMVar :: IO (MVar (State -> State))
-  void $ forkIO (monitorCharging zone updater)
-  void $ forkIO (monitorCapacity updater)
   void $ forkIO (monitorSound updater)
+  void $ forkIO (monitorChargingAndSuspend zone updater)
+  void $ forkIO (monitorCapacity updater)
   void $ forkIO (monitorTime updater)
   hSetBuffering stdout LineBuffering
   let printer state = do
@@ -43,12 +43,14 @@ main = do
           printer new
   printer initialState
 
-getTime :: TimeZone -> IO Time
-getTime zone = do
-  now <- getCurrentTime
-  let (LocalTime x (TimeOfDay h m _)) = utcToLocalTime zone now
-      (y,mo,d) = toGregorian x
-  pure $ Time (fromIntegral y) mo d h m
+----
+
+monitorSound :: MVar (State -> State) -> IO ()
+monitorSound mv = withCreateProcess (proc "alsactl" ["monitor"]) {std_out = CreatePipe} $ \_ (Just hout) _ _ ->
+  forever $ do
+    void $ B8.hGetLine hout
+    x <- getSound
+    putMVar mv (\s -> s{sound = x})
 
 getSound :: IO Sound
 getSound = A.withMixer "default" $ \mixer -> do
@@ -60,20 +62,10 @@ getSound = A.withMixer "default" $ \mixer -> do
     Just vol <- A.getChannel A.FrontLeft $ A.value $ playbackVolume
     pure $ Sound (fromIntegral $ vol*100 `div` hi) (not sw)
 
-monitorTime :: MVar (State -> State) -> IO ()
-monitorTime mv = forever $ do
-  threadDelay 60000000
-  putMVar mv (\s -> s{time = (time s) {minute = succ . minute $ time s}})
+----
 
-monitorCapacity :: MVar (State -> State) -> IO ()
-monitorCapacity mv = withFile "/sys/class/power_supply/BAT0/capacity" ReadMode $ \fh -> forever $ do
-  threadDelay 300000000  -- 5 min
-  newCapacity <- B8.hGetLine fh
-  putMVar mv (\s -> s{battery = (battery s) {capacity = newCapacity}})
-  hSeek fh AbsoluteSeek 0
-
-monitorCharging :: TimeZone -> MVar (State -> State) -> IO ()
-monitorCharging zone mv = withCreateProcess (proc "udevadm" ["monitor", "-k", "-s", "usb_power_delivery", "-s", "nvme"]) {std_out = CreatePipe} $ \_ (Just hout) _ _ -> do
+monitorChargingAndSuspend :: TimeZone -> MVar (State -> State) -> IO ()
+monitorChargingAndSuspend zone mv = withCreateProcess (proc "udevadm" ["monitor", "-k", "-s", "usb_power_delivery", "-s", "nvme"]) {std_out = CreatePipe} $ \_ (Just hout) _ _ -> do
   xs <- words <$> hGetContents hout
   forM_ xs $ \case
     "add" -> update True
@@ -84,28 +76,39 @@ monitorCharging zone mv = withCreateProcess (proc "udevadm" ["monitor", "-k", "-
     _ -> pure ()
     where update b = putMVar mv (\s -> s{battery = (battery s) {charging = b}})
 
-monitorSound :: MVar (State -> State) -> IO ()
-monitorSound mv = withCreateProcess (proc "alsactl" ["monitor"]) {std_out = CreatePipe} $ \_ (Just hout) _ _ ->
-  forever $ do
-    void $ B8.hGetLine hout
-    x <- getSound
-    putMVar mv (\s -> s{sound = x})
+getCharging :: IO Bool
+getCharging = not . isPrefixOf "Discharging" <$> readFile "/sys/class/power_supply/BAT0/status"
+
+----
+
+monitorCapacity :: MVar (State -> State) -> IO ()
+monitorCapacity mv = withFile "/sys/class/power_supply/BAT0/capacity" ReadMode $ \fh -> forever $ do
+  threadDelay 300000000  -- 5 min
+  newCapacity <- B8.hGetLine fh
+  putMVar mv (\s -> s{battery = (battery s) {capacity = newCapacity}})
+  hSeek fh AbsoluteSeek 0
 
 getCapacity :: IO B8.ByteString
 getCapacity = B8.dropEnd 1 <$> B8.readFile "/sys/class/power_supply/BAT0/capacity"
 
-getStatus :: IO Bool
-getStatus = not . isPrefixOf "Discharging" <$> readFile "/sys/class/power_supply/BAT0/status"
-
 getBattery :: IO Battery
-getBattery = Battery <$> getCapacity <*> getStatus
+getBattery = Battery <$> getCapacity <*> getCharging
 
-{-# INLINE prettyTime #-}
-prettyTime :: Time -> Builder
-prettyTime Time{..} = intDec year <> char7 '.' <> intDec month <> char7 '.' <> intDec day <> char7 ' ' <> intDec hour <> char7 ':' <> pad minute
-  where {-# INLINE pad #-}
-        pad i | i < 10 = char7 '0' <> intDec i
-        pad i = intDec i
+----
+
+monitorTime :: MVar (State -> State) -> IO ()
+monitorTime mv = forever $ do
+  threadDelay 60000000
+  putMVar mv (\s -> s{time = (time s) {minute = succ . minute $ time s}})
+
+getTime :: TimeZone -> IO Time
+getTime zone = do
+  now <- getCurrentTime
+  let (LocalTime x (TimeOfDay h m _)) = utcToLocalTime zone now
+      (y,mo,d) = toGregorian x
+  pure $ Time (fromIntegral y) mo d h m
+
+----
 
 {-# INLINE prettySound #-}
 prettySound :: Sound -> Builder
@@ -126,6 +129,15 @@ prettyBattery Battery{..} = text <> pad num
           1 -> (char7 ' ' <>) . (char7 ' ' <>)
           2 -> (char7 ' ' <>)
           _ -> id
+
+{-# INLINE prettyTime #-}
+prettyTime :: Time -> Builder
+prettyTime Time{..} = intDec year <> char7 '.' <> intDec month <> char7 '.' <> intDec day <> char7 ' ' <> intDec hour <> char7 ':' <> pad minute
+  where {-# INLINE pad #-}
+        pad i | i < 10 = char7 '0' <> intDec i
+        pad i = intDec i
+
+----
 
 prettyState :: State -> Builder
 prettyState (State{..}) = prettySound sound <> separator <> prettyBattery battery <> separator <> prettyTime time <> char7 '\n'
